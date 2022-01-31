eea239f6b27c31845c309a81df171d023b213fb6
Ezra <ezragregoreo@gmail.com>
2022-01-31 20:58:26 +0900

    Responsive Home Page

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,14 @@
+console.log("hello")
 
+const navLinks = document.getElementById("navLinks");
+const faBars = document.getElementById("faBars");
+
+function showMenu() {
+    navLinks.style.display= "inline";
+    faBars.style.display= "none";
+};
+
+function hideMenu() {
+    navLinks.style.display= "none";
+    faBars.style.display= "inline";
+}
